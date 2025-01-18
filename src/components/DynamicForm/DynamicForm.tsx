81ed096ee59564a619a3b033809e6e819b222a98
dassajib <sajibdas.sd@gmail.com>
2025-01-19 04:24:41 +0600

    item api int WIP. Table code spilited

--- src/components/DynamicForm/DynamicForm.tsx
+++ src/components/DynamicForm/DynamicForm.tsx
@@ -1,66 +1,3 @@
-// import React, { useEffect } from 'react';
-// import { useForm, SubmitHandler } from 'react-hook-form';
-
-// interface InputConfig {
-//   name: string;
-//   type: string;
-//   label: string;
-//   placeholder: string;
-//   required?: boolean;
-// }
-
-// interface DynamicFormProps {
-//   inputs: InputConfig[];
-//   onSubmit: SubmitHandler<any>;
-//   defaultValues?: Record<string, any>;
-// }
-
-// const DynamicForm: React.FC<DynamicFormProps> = ({
-//   inputs,
-//   onSubmit,
-//   defaultValues,
-// }) => {
-//   const { register, handleSubmit, setValue } = useForm({
-//     defaultValues,
-//   });
-
-//   useEffect(() => {
-//     if (defaultValues) {
-//       for (const key in defaultValues) {
-//         setValue(key, defaultValues[key]);
-//       }
-//     }
-//   }, [defaultValues, setValue]);
-
-//   return (
-//     <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 mt-8">
-//       {inputs.map((input) => (
-//         <div key={input.name}>
-//           <label className="block text-sm font-medium dark:text-[#ECEDEF] text-[#1C2434]">
-//             {input.label}
-//           </label>
-//           <input
-//             {...register(input.name, { required: input.required })}
-//             type={input.type}
-//             placeholder={input.placeholder}
-//             className="bg-white dark:bg-[#24303F] dark:text-white text-gray-900 mt-1 block w-full shadow-sm sm:text-sm border-[1.5px] border-gray-300 rounded-md p-3 focus:ring-2 focus:ring-blue-500"
-//           />
-//         </div>
-//       ))}
-//       <div className="flex justify-center">
-//         <button
-//           type="submit"
-//           className="mt-6 px-4 py-2 dark:bg-[#3B4ED1] bg-[#3C50E0] hover:bg-blue-700 text-white font-semibold rounded-md transition duration-300"
-//         >
-//           Submit
-//         </button>
-//       </div>
-//     </form>
-//   );
-// };
-
-// export default DynamicForm;
-
 import React, { useEffect } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { Select } from 'antd';
@@ -105,15 +42,6 @@ const DynamicForm: React.FC<DynamicFormProps> = ({
             {input.label}
           </label>
           {input.type === 'select' ? (
-            // <Select
-            //   {...register(input.name, { required: input.required })}
-            //   showSearch
-            //   placeholder={input.placeholder}
-            //   className="w-full"
-            //   options={input.options?.map((option) => ({
-            //     label: option,
-            //     value: option,
-            //   }))}
             <Select
               {...register(input.name, { required: input.required })}
               showSearch
